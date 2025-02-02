bd3a621a9506fde835f73e7964ac9b56a77d725b
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-02 13:26:21 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,9 @@
+//Task 1: Product Price Management Scenario: Online Store You are managing a product pricing system
+
+let prices = [10,40,25,65,80]; // this line is assigning the variable prices to an array with numbers in it
+
+prices.push(95); // this line is adding the number 95 to the end of the array
+prices.splice(0,1,); // this line is deleting the first value (10) in the array
+
+console.log("The prices are:", prices); // this line logs the array prices in the console
+
